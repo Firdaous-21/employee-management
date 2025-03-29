22a0c73450e7cc43b503390160bb3dd9f7516371
firdaous <firdaous.charchaoui@gmail.com>
2025-03-29 11:15:32 +0000

    Initial commit

---
+++ src/test/java/com/master/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
